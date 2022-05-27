680db4e43ae8a97dc3448400c378d1c380053699
Oliveira <caroline.oliveira@capgemini.com>
2022-05-27 10:34:55 -0300

    atualizei o entity na instituição para o projeto rodar

--- src/main/java/agaluno/mvc/entidades/Instituicao.java
+++ src/main/java/agaluno/mvc/entidades/Instituicao.java
@@ -1,7 +1,14 @@
 package agaluno.mvc.entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Instituicao {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String nome;
 	private String sigla;
