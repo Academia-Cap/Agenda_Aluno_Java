408ac91762f6c5a24fb18347a71cf8194785a91e
Rocha <tania.rocha@capgemini.com>
2022-05-27 09:39:18 -0300

    Agenda Aluno Java - Estrutura

---
+++ src/main/java/agaluno/mvc/AgendaAlunoApplication.java
@@ -0,0 +1,13 @@
+package agaluno.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaAlunoApplication.class, args);
+	}
+
+}
