b7b6ec7bfba69a8e8e5f19fbf613f309f694d3a6
Oliveira <caroline.oliveira@capgemini.com>
2022-05-31 01:52:58 -0300

    adicionei a entidade tarefa

---
+++ src/main/java/agaluno/mvc/DTO/TarefaDTO.java
@@ -0,0 +1,5 @@
+package agaluno.mvc.DTO;
+
+public class TarefaDTO {
+
+}
---
+++ src/main/java/agaluno/mvc/entidades/Tarefa.java
@@ -0,0 +1,73 @@
+package agaluno.mvc.entidades;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Tarefa {
+	private int id;
+	private Date periodo;
+	private Time horaInicio;
+	private Time horaFinal;
+	private Disciplina disciplina;
+	private Aluno aluno;
+
+	public Tarefa() {
+	}
+
+	public Tarefa(int id, Date periodo, Time horaInicio, Time horaFinal, Disciplina disciplina, Aluno aluno) {
+		this.id = id;
+		this.periodo = periodo;
+		this.horaInicio = horaInicio;
+		this.horaFinal = horaFinal;
+		this.disciplina = disciplina;
+		this.aluno = aluno;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getPeriodo() {
+		return periodo;
+	}
+
+	public void setPeriodo(Date periodo) {
+		this.periodo = periodo;
+	}
+
+	public Time getHoraInicio() {
+		return horaInicio;
+	}
+
+	public void setHoraInicio(Time horaInicio) {
+		this.horaInicio = horaInicio;
+	}
+
+	public Time getHoraFinal() {
+		return horaFinal;
+	}
+
+	public void setHoraFinal(Time horaFinal) {
+		this.horaFinal = horaFinal;
+	}
+
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
+
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+
+	public Aluno getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+}
