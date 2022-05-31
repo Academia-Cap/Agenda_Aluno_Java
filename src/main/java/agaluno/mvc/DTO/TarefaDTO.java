2b3d984165a7f7782dfb39fd234403d6f2879f3e
Oliveira <caroline.oliveira@capgemini.com>
2022-05-31 01:56:41 -0300

    adicionei o DTO da tarefa

--- src/main/java/agaluno/mvc/DTO/TarefaDTO.java
+++ src/main/java/agaluno/mvc/DTO/TarefaDTO.java
@@ -1,5 +1,67 @@
 package agaluno.mvc.DTO;
 
-public class TarefaDTO {
+import java.sql.Date;
+import java.sql.Time;
 
+import agaluno.mvc.entidades.Aluno;
+import agaluno.mvc.entidades.Disciplina;
+import agaluno.mvc.entidades.Tarefa;
+
+public class TarefaDTO {
+	private Date periodo;
+	private Time horaInicio;
+	private Time horaFinal;
+	private Disciplina disciplina;
+	private Aluno aluno;
+
+	public TarefaDTO() {
+	}
+
+	public TarefaDTO(Tarefa tarefa) {
+		this.periodo = tarefa.getPeriodo();
+		this.horaInicio = tarefa.getHoraInicio();
+		this.horaFinal = tarefa.getHoraFinal();
+		this.disciplina = tarefa.getDisciplina();
+		this.aluno = tarefa.getAluno();
+	}
+
+	public Date getPeriodo() {
+		return periodo;
+	}
+
+	public void setPeriodo(Date periodo) {
+		this.periodo = periodo;
+	}
+
+	public Time getHoraInicio() {
+		return horaInicio;
+	}
+
+	public void setHoraInicio(Time horaInicio) {
+		this.horaInicio = horaInicio;
+	}
+
+	public Time getHoraFinal() {
+		return horaFinal;
+	}
+
+	public void setHoraFinal(Time horaFinal) {
+		this.horaFinal = horaFinal;
+	}
+
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
+
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+
+	public Aluno getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
 }
